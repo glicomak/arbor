b1b8b45d87a57496c94ca39f130453338e91ca68
glicomak <skewtron@gmail.com>
2025-09-26 21:42:21 +0530

    Add GET active courses endpoint and page

--- nuxt-app/app/pages/index.vue
+++ nuxt-app/app/pages/index.vue
@@ -1,3 +1,21 @@
+<script setup>
+const { data } = useFetch("/api/courses");
+</script>
+
 <template>
-  <h1>Welcome back to Arbor!</h1>
+  <h1>Courses</h1>
+  <div class="grid grid-cols-4 gap-4">
+    <Card v-for="course in data">
+      <template #title>{{ course.name }}</template>
+      <template #subtitle>{{ course.program.name }}</template>
+      <template #content>
+        <p class="m-0">{{ course.description }}</p>
+      </template>
+      <template #footer>
+        <div class="mt-1">
+          <Button label="Open" class="w-full" />
+        </div>
+      </template>
+    </Card>
+  </div>
 </template>
