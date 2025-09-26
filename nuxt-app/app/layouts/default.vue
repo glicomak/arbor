43560cb2b0a855337cf7b57d9d2a2237e155f6c5
glicomak <skewtron@gmail.com>
2025-09-26 20:20:36 +0530

    Make navbar functional

--- nuxt-app/app/layouts/default.vue
+++ nuxt-app/app/layouts/default.vue
@@ -1,11 +1,19 @@
 <script setup>
 const items = ref([
-  { label: "Arbor" },
-  { label: "Programs" }
+  { label: "Arbor", route: "/" },
+  { label: "Programs", route: "/programs" }
 ]);
 </script>
 
 <template>
-  <Menubar :model="items" class="font-medium" />
+  <Menubar :model="items" class="font-medium">
+    <template #item="{ item, props }">
+      <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+        <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+          <span>{{ item.label }}</span>
+        </a>
+      </router-link>
+    </template>
+  </Menubar>
   <slot />
 </template>
